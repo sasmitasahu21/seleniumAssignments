9f18d7f97318b1c565b9adb15f4fa1cc15851db7
sasmitasahu21 <154872570+sasmitasahu21@users.noreply.github.com>
2023-12-26 13:08:46 +0530

    Add files via upload

---
+++ WEEK1DAY1/AssignmentBrowser.java
@@ -0,0 +1,10 @@
+package WEEK1DAY1;
+
+public class AssignmentBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ WEEK1DAY1/Firefox.java
@@ -0,0 +1,19 @@
+package WEEK1DAY1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float browserVersion = 100.2f;
+String browserName = "Firefox";
+boolean isVisible = true;
+int releaseYear = 1998;
+char browserLogo = 'f';
+System.out.println("Browser Version : " +browserVersion);
+System.out.println("Browser Name : " +browserName);
+System.out.println("isVisible? : " +isVisible);
+System.out.println("ReleaseYear : " +releaseYear);
+System.out.println("BrowserLogo : " +browserLogo);
+	}
+
+}
