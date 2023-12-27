24e39d3380f0ab353c2f56749fd635a2b2f2f322
sasmitasahu21 <154872570+sasmitasahu21@users.noreply.github.com>
2023-12-27 18:43:33 +0530

    Add files via upload

---
+++ SampleSelenium.java
@@ -0,0 +1,14 @@
+package Week2Day1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SampleSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+
+	}
+
+}
